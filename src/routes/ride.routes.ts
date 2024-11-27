1c0b018aa7fb0f7755b1967ec8283b2a8a7a5ce9
Vitor Nogueira <vleonel96@gmail.com>
2024-11-27 07:33:20 -0300

    feat: add route to retrieve driver records

--- src/routes/ride.routes.ts
+++ src/routes/ride.routes.ts
@@ -1,12 +1,15 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
 
 import driversController from "../controllers/ride.controller";
-import { hasCustomerBody, hasAddress, addressIsTheSame, hasDriver, hasDistance } from "../middlewares/ride.middleware";
+import { hasCustomerBody, hasAddress, addressIsTheSame, hasDriver, hasDistance } from "../middlewares/ride.middleware"
 
 const driverRoutes = express.Router();
 
 driverRoutes.post("/estimate", hasCustomerBody, hasAddress, addressIsTheSame, driversController.getRideEstimates);
 
 driverRoutes.patch('/confirm', hasCustomerBody, hasAddress, addressIsTheSame, hasDriver, hasDistance, driversController.confirmRide);
+
+driverRoutes.get('/drivers', driversController.getDrivers);
+
 
 export default driverRoutes;
