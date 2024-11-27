547198b1a8755b06a2b2a40eb8d97a406f47c4b7
Vitor Nogueira <vleonel96@gmail.com>
2024-11-27 10:34:49 -0300

    feat: integrate routing and add Toaster for notifications in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,33 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { Toaster } from 'sonner';
+import { useTheme } from 'next-themes';
+
+import Home from './pages/Home';
+
+import Layout from './components/Layout';
 
 import { store } from './store/store';
 
+import './globals.css';
+
+type ThemeProps = {
+  theme: 'light' | 'dark' | 'system';
+}
+
 const App: React.FC = () => {
+  const { theme } = useTheme() as unknown as ThemeProps;
   return (
     <Provider store={store}>
-      <section>
-        Hello World
-      </section>
+      <Toaster richColors visibleToasts={1} position="top-center" theme={theme} />
+      <Router>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </Layout>
+      </Router>
     </Provider>
   );
 };
