a2f495eae4e8a893f21527ac4ebe8c51ff4232de
Vitor Nogueira <vleonel96@gmail.com>
2024-11-27 10:07:12 -0300

    chore: initialize project with React, TypeScript, and Vite setup; add ESLint configuration and .gitignore

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './globals.css';
+
+const App: React.FC = () => {
+  return (
+    <section>
+      Hello World
+    </section>
+  );
+};
+
+export default App;
+
---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tsconfigPaths from "vite-tsconfig-paths"
+
+export default defineConfig({
+  plugins: [react(), tsconfigPaths()],
+  server: {
+    host: true,
+    port: 80,
+  },
+  preview: {
+    port: 80,
+    host: true,
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom']
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1000
+  },
+})
