d0818861dd471a89d480c29bfd5be0d9541ac406
Vitor Nogueira <vleonel96@gmail.com>
2024-11-27 07:18:10 -0300

    feat: add formatDrivers utility function to format driver data

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -1,7 +1,24 @@
+import { IDriver } from "../../src/interfaces/IDriver";
+
 const convertMetersToKm = (meters: number) => {
   return meters / 1000;
 }
 
+const formatDrivers = (drivers: IDriver[]) =>
+  drivers.map(driver => ({
+    id: driver.id,
+    name: driver.name,
+    description: driver.description,
+    vehicle: driver.vehicle,
+    review: {
+      rating: driver.rating,
+      comment: driver.comment,
+    },
+    value: driver.value,
+  }));
+
+
 export default {
-  convertMetersToKm
+  convertMetersToKm,
+  formatDrivers,
 }
