d963b60c8a67329fc213077daee5ee69ef953ba6
Vitor Nogueira <vleonel96@gmail.com>
2024-11-27 07:05:24 -0300

    feat: add constants for error codes and status codes

---
+++ src/utils/constants.ts
@@ -0,0 +1,20 @@
+export const ERROR_CODE_INVALID_DATA = "INVALID_DATA"
+export const DESCRIPTION_INVALID_DATA = "Os dados fornecidos no corpo da requisição são inválidos";
+
+export const ERROR_CODE_DRIVER_NOT_FOUND = "DRIVER_NOT_FOUND";
+export const DESCRIPTION_DRIVER_NOT_FOUND = "Motorista não encontrado";
+
+export const ERROR_CODE_INVALID_DISTANCE = "INVALID_DISTANCE";
+export const DESCRIPTION_INVALID_DISTANCE = "Quilometragem inválida para o motorista";
+
+export const ERROR_CODE_INVALID_DRIVER = "INVALID_DRIVER";
+export const DESCRIPTION_INVALID_DRIVER = "Motorista inválido";
+
+export const ERROR_CODE_RIDE_NOT_FOUND = "NO_RIDES_FOUND";
+export const DESCRIPTION_RIDE_NOT_FOUND = "Nenhum registro encontrado";
+
+export const STATUS_CODE_OK = 200;
+export const STATUS_CODE_BAD_REQUEST = 400;
+export const STATUS_CODE_NOT_ACCEPTABLE = 406;
+export const STATUS_CODE_INTERNAL_SERVER_ERROR = 500;
+export const STATUS_CODE_NOT_FOUND = 404;
