4c8583897ec462b94acec71e4b98adc3a4ae7f0c
Vitor Nogueira <vleonel96@gmail.com>
2024-11-27 07:27:51 -0300

    feat: add formatDuration function to convert duration from seconds to a readable format and decodedPolyline

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -1,8 +1,27 @@
+import helpers from "@googlemaps/polyline-codec";
 import { IDriver } from "../../src/interfaces/IDriver";
+
+const decodedPolyline = (encodedPolyline: string) => helpers.decode(encodedPolyline);
 
 const convertMetersToKm = (meters: number) => {
   return meters / 1000;
 }
+
+const formatDuration = (durationInSeconds: string): string => {
+  const totalSeconds = parseInt(durationInSeconds.replace('s', ''), 10);
+
+  const hours = Math.floor(totalSeconds / 3600);
+  const minutes = Math.floor((totalSeconds % 3600) / 60);
+  const seconds = totalSeconds % 60;
+
+  if (hours > 0) {
+    return `${hours}h ${minutes}m`;
+  } else if (minutes > 0) {
+    return seconds > 0 ? `${minutes}m ${seconds}s` : `${minutes}m`;
+  } else {
+    return `${seconds}s`;
+  }
+};
 
 const formatDrivers = (drivers: IDriver[]) =>
   drivers.map(driver => ({
@@ -17,8 +36,9 @@ const formatDrivers = (drivers: IDriver[]) =>
     value: driver.value,
   }));
 
-
 export default {
+  decodedPolyline,
   convertMetersToKm,
-  formatDrivers,
+  formatDuration,
+  formatDrivers
 }
