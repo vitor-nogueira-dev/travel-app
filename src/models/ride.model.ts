dbb5c12f73672091e64e1301121693e409d8659a
Vitor Nogueira <vleonel96@gmail.com>
2024-11-27 07:36:49 -0300

    feat: add getRidesByCustomerIdAndDriverId function to retrieve rides based on customer and optional driver ID

--- src/models/ride.model.ts
+++ src/models/ride.model.ts
@@ -1,6 +1,7 @@
 import { executeQuery } from "../../src/utils/executeQuery";
+import functions from "../../src/utils/functions";
 
-import { IRideSave } from "../../src/interfaces/IRide";
+import { IRide, IRawRide, IRideSave } from "../../src/interfaces/IRide";
 
 const saveRide = async (ride: IRideSave) => {
   const date = new Date();
@@ -18,4 +19,41 @@ const saveRide = async (ride: IRideSave) => {
   );
 };
 
-export default { saveRide };
+const getRidesByCustomerIdAndDriverId = async (
+  customerId: number,
+  driverId?: number
+): Promise<IRide[]> => {
+  const baseQuery = `
+    SELECT 
+      r.id,
+      r.date,
+      r.origin,
+      r.destination,
+      r.distance,
+      r.duration,
+      d.id AS driver_id,
+      d.name AS driver_name,
+      r.value
+    FROM 
+      rides r
+    ${driverId ? 'LEFT' : 'INNER'} JOIN 
+      drivers d ON r.driver_id = d.id
+    WHERE 
+      r.customer_id = ?
+      ${driverId ? 'AND r.driver_id = ?' : ''}
+    ORDER BY 
+      r.date DESC
+  `;
+
+  const queryParams = driverId ? [customerId, driverId] : [customerId];
+
+  const rides = await executeQuery<IRawRide>(
+    baseQuery,
+    queryParams
+  );
+
+  return rides.map(functions.transformRideData);
+};
+
+
+export default { saveRide, getRidesByCustomerIdAndDriverId };
