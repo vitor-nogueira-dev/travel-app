aa627efe5f9458b27f8889fa42a9f453e1afd756
Vitor Nogueira <vleonel96@gmail.com>
2024-11-27 07:30:29 -0300

    feat: add confirm ride route with validation middleware for driver and distance

--- src/routes/ride.routes.ts
+++ src/routes/ride.routes.ts
@@ -1,10 +1,12 @@
 import express from "express";
 
 import driversController from "../controllers/ride.controller";
-import { hasCustomerBody, hasAddress, addressIsTheSame } from "../middlewares/ride.middleware";
+import { hasCustomerBody, hasAddress, addressIsTheSame, hasDriver, hasDistance } from "../middlewares/ride.middleware";
 
 const driverRoutes = express.Router();
 
 driverRoutes.post("/estimate", hasCustomerBody, hasAddress, addressIsTheSame, driversController.getRideEstimates);
+
+driverRoutes.patch('/confirm', hasCustomerBody, hasAddress, addressIsTheSame, hasDriver, hasDistance, driversController.confirmRide);
 
 export default driverRoutes;
