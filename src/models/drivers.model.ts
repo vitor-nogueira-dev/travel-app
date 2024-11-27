43de7d45e5a42e2166d4e8e37ef77f8aea5f69e0
Vitor Nogueira <vleonel96@gmail.com>
2024-11-27 07:17:21 -0300

    feat: add getDriversByMinKm function to retrieve drivers based on minimum distance

---
+++ src/models/drivers.model.ts
@@ -0,0 +1,23 @@
+import { executeQuery } from "../../src/utils/executeQuery";
+
+import { IDriverRaw } from "../../src/interfaces/IDriver";
+
+const getDriversByMinKm = async (km: number): Promise<IDriverRaw[]> => {
+  return executeQuery<IDriverRaw>(
+    `SELECT 
+      *,
+      'Avaliação disponível' AS comment, -- Placeholder para o campo de comentário
+      GREATEST(min_distance_km, ?) * rate_per_km AS value
+    FROM 
+      drivers
+    WHERE 
+      min_distance_km <= ?
+    ORDER BY 
+      value ASC;`,
+    [km, km]
+  );
+}
+
+export default {
+  getDriversByMinKm,
+};
