e0d638efb24da88cc1b40fba6afb6d0929c79a42
Vitor Nogueira <vleonel96@gmail.com>
2024-11-27 11:58:00 -0300

    refactor: remove unnecessary urlencoded middleware from app configuration

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -18,7 +18,6 @@ const access: express.RequestHandler = (_, res, next) => {
 }
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(cors());
 app.use(access);
